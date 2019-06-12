084a129b67896d3def5a2b3e7b9373c26495ba2d
Marek Fišera <mara@neptuo.com>
2019-06-12 18:38:10 +0200

    Deploy blazor-v0.1.0.

---
+++ js/site.js
@@ -0,0 +1,43 @@
+﻿window.Bootstrap = {
+    Modal: {
+        Register: function (id) {
+            var target = $("#" + id);
+            target.on('shown.bs.modal', function (e) {
+                $(e.currentTarget).find('[data-autofocus]').select().focus();
+            });
+            target.on('hidden.bs.modal', function (e) {
+                DotNet.invokeMethodAsync("Recollection.Blazor.UI", "Bootstrap_ModalHidden", e.currentTarget.id);
+            });
+
+            return true;
+        },
+        Toggle: function (id, isVisible) {
+            var target = $("#" + id);
+            target.modal(isVisible ? 'show' : 'hide');
+
+            return true;
+        }
+    }
+};
+
+window.Recollection = {
+    NavigateTo: function (href) {
+        window.location.href = href;
+        return true;
+    },
+    SaveToken: function (token) {
+        if ("localStorage" in window) {
+            if (token == null)
+                window.localStorage.removeItem("token");
+            else
+                window.localStorage.setItem("token", token);
+        }
+    },
+    LoadToken: function () {
+        if ("localStorage" in window) {
+            return window.localStorage.getItem("token");
+        }
+
+        return null;
+    }
+};
